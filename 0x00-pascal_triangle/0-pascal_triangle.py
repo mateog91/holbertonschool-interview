d12c372837fb6732721a33cfbaae885d8ffa6f61
Mateo <mateog91@gmail.com>
2022-04-14 11:00:28 -0500

    ADD: file 0

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -5,6 +5,8 @@ Module for pascal triangle function
 
 
 def pascal_triangle(n):
+    if n <= 0:
+        return []
     # initial result
     result = [[1]]
 
@@ -14,9 +16,6 @@ def pascal_triangle(n):
         temp_row = [0] + result[-1] + [0]
         # calculate length of new row
         new_row_length = len(result[-1]) + 1
-        # new_row = []
-        # for j in range(new_row_length):
-        #     new_row.append(temp_row[j] + temp_row[j + 1])
 
         new_row = [temp_row[j] + temp_row[j + 1]
                    for j in range(new_row_length)]
