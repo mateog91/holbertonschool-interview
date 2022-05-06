01563cd96995b79282fa7c72542360521eda793c
Mateo <mateog91@gmail.com>
2022-05-05 22:38:11 -0500

    added system exit

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -14,12 +14,12 @@ if __name__ == '__main__':
         [print("{}: {}".format(tup[0], tup[1]))
          for tup in list_tupples if tup[1] > 0]
 
+    # initialize variables
+    possible_status_codes = [200, 301, 400, 401, 403, 404, 405, 500]
+    status_code_counts = {element: 0 for element in possible_status_codes}
+    total_file_size = 0
+    count = 0
     try:
-        # initialize variables
-        possible_status_codes = [200, 301, 400, 401, 403, 404, 405, 500]
-        status_code_counts = {element: 0 for element in possible_status_codes}
-        total_file_size = 0
-        count = 0
         # catch input line
         for line in sys.stdin:
             # count and split line
@@ -27,18 +27,18 @@ if __name__ == '__main__':
             count += 1
 
             # check line has correct format
-            if len(line) == 9:
-                if count != 0 and count % 10 == 0:
-                    statistics(status_code_counts, total_file_size)
-                try:
-                    status_code, file_size = int(line[-2]), int(line[-1])
 
-                    if status_code in possible_status_codes:
-                        total_file_size += file_size
-                        status_code_counts[status_code] += 1
+            if count != 0 and count % 10 == 0:
+                statistics(status_code_counts, total_file_size)
+            try:
+                status_code, file_size = int(line[-2]), int(line[-1])
 
-                except TypeError:
-                    pass
-    except KeyboardInterrupt:
+                if status_code in possible_status_codes:
+                    total_file_size += file_size
+                    status_code_counts[status_code] += 1
+
+            except Exception:
+                pass
+    except (KeyboardInterrupt, SystemExit):
         statistics(status_code_counts, total_file_size)
         raise
